01893d6f07deb6b978417e2f10355e09d3d0056e
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-20 01:08:21 -0400

    updates

--- src/components/PropSlalom.tsx
+++ src/components/PropSlalom.tsx
@@ -19,32 +19,38 @@ export const PropSlalom = ({
   image,
   side = "left",
 }: Props) => {
+  // TODO: FIX THIS TWMERGE
   return (
-    <div className="py-24">
+    <div className="py-24 px-4 md:px-12 lg:px-4">
       <div
-        className={`w-full flex justify-center items-center ${
+        className={`w-full flex flex-col md:flex-row md:gap-8 lg:gap-0 justify-center items-center ${
           side === "left" ? null : twMerge("flex-row-reverse")
         } `}
       >
-        <div className="flex-1 flex flex-col">
-          <h2 className="relative font-crimson font-bold text-[144px] leading-[.877em] w-max">
+        <div className="xl:flex-1 flex flex-col">
+          <h2 className="relative font-crimson font-bold text-[88px] xl:text-[144px] leading-[.877em] w-max">
             {title}
             <div className="absolute -z-10 h-2.5 w-[72%] bottom-0 right-2 bg-red rounded-full"></div>
           </h2>
-          <div className="w-[80%] space-y-4 pl-14">
-            <p className="pt-8 text-[24px] leading-[1.5em] tracking-[.02em] ">
+          <div className="lg:w-[80%] space-y-4 md:pl-8 md:pr-4 lg:pl-14 lg:pr-0">
+            <p className="pt-8 text-[16px] xl:text-[24px] leading-[1.5em] tracking-[.02em] ">
               {description}
             </p>
             <a
               href={link.url}
-              className="block font-crimson text-[32px] tracking-[.02em]"
+              className="block font-crimson text-[20px] lg:text-[32px] tracking-[.02em]"
             >
               {link.label}
             </a>
           </div>
         </div>
-        <div className="">
-          <img src={image} alt={`${title} image`} />
+        <div className="relative">
+          <img
+            src={image}
+            alt={`${title} image`}
+            className="shadow-md max-w-[355px] w-[40vw] md:w-[30vw] h-auto"
+          />
+          <div className="absolute -z-10 -right-6 -top-4 h-full w-full border-2 border-dark rounded-2xl"></div>
         </div>
       </div>
     </div>
--- src/components/StaffInfo.tsx
+++ src/components/StaffInfo.tsx
@@ -19,30 +19,38 @@ export const StaffInfo = ({
   side = "left",
 }: Props) => {
   return (
-    <div>
+    <div className="py-12 px-4">
       <div
-        className={`flex gap-[30px] items-center max-w-[868px] ${
+        className={`flex gap-8 lg:gap-[30px] items-center max-w-[868px] ${
           side === "left" ? twMerge("flex-row") : twMerge("flex-row-reverse")
         }`}
       >
-        <img src={image} alt="image of person" />
+        <img
+          src={image}
+          alt="image of person"
+          className="w-52 h-auto lg:w-full"
+        />
         <div className="space-y-4">
           <div
-            className={`flex flex-col gap-4 relative header ${
+            className={`flex flex-col gap-1 lg:gap-4 relative header ${
               side === "left"
                 ? ""
-                : twMerge("items-end before:left-0 before:right-[-10%]")
+                : twMerge("items-end before:left-0 before:right-[-30%]")
             }`}
           >
             <h3
-              className={`flex gap-3 text-[56px] leading-[.877em] ${
+              className={`flex gap-3 text-[42px] lg:text-[56px] leading-[.877em] ${
                 side === "left" ? twMerge("row") : twMerge("flex-row-reverse")
               }`}
             >
               {title}
-              <img src={icon ?? "/knife.svg"} alt="icon image" />
+              <img
+                src={icon ?? "/knife.svg"}
+                alt="icon image"
+                className="w-10 h-auto"
+              />
             </h3>
-            <h4 className="font-crimson font-bold text-[48px] leading-[.877em]">
+            <h4 className="font-crimson font-bold text-[32px] lg:text-[48px] leading-[.877em]">
               {name}
             </h4>
           </div>
