0c31bf4066bab0cfd21bd9a86ac75d4c41d83dcb
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-29 16:33:24 -0400

    tryna make this prod 1

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,50 @@
+// tina/config.ts
+import { defineConfig } from "tinacms";
+var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+var config_default = defineConfig({
+  branch,
+  clientId: process.env.NEXT_PUBLIC_TINA_ID,
+  // Get this from tina.io
+  token: process.env.NEXT_PUBLIC_TINA_TOKEN,
+  // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public"
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public"
+    }
+  },
+  schema: {
+    collections: [
+      {
+        name: "menu",
+        label: "Menu Items",
+        path: "content/menu",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Menu Item",
+            required: true
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Item Description"
+          },
+          {
+            type: "string",
+            name: "price",
+            label: "Price"
+          }
+        ]
+      }
+    ]
+  }
+});
+export {
+  config_default as default
+};
