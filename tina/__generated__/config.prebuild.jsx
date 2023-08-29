6ec2bf76ec3b632fb04b483dba0b8bd666d22008
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-29 00:38:53 -0400

    more env

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -2,7 +2,7 @@
 import { defineConfig } from "tinacms";
 var TINA_ID = process.env.TINA_ID;
 var TINA_TOKEN = process.env.TINA_TOKEN;
-var branch = process.env.HEAD || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || "main";
+var branch = process.env.VERCEL_BRANCH_URL || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || process.env.VERCEL_URL || "main";
 var config_default = defineConfig({
   branch,
   clientId: TINA_ID,
