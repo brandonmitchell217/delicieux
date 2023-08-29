249d6f1af6e26eae1ceee769e1bc76af85196772
Brandon Mitchell <brandonmitchell217@gmail.com>
2023-08-28 23:34:05 -0400

    [feat]: fetch done

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TestComponent({ data }: any) {
+  //   console.log(data);
+  return (
+    <div>
+      {data.map((item: any, index: number) => (
+        <div key={index}>
+          <h2>{item.node.title}</h2>
+          <p>{item.node.price}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
